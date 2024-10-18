1b0a81482ad0f7b73d9148842065023aa1b83da1
anon <anon@local>
2024-10-17 22:11:33 -0300

    Exec with no rows

--- db/rows.go
+++ db/rows.go
@@ -3,10 +3,17 @@ package db
 import (
 	"context"
 	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/pgconn"
 )
 
 var GetRows RawExecFunc[pgx.Rows] = getRows
 
 func getRows(ctx context.Context, executor Executor, stmt string, args ...any) (pgx.Rows, error) {
 	return executor.Query(ctx, stmt, args...)
+}
+
+var Exec RawExecFunc[pgconn.CommandTag] = execNoRows
+
+func execNoRows(ctx context.Context, executor Executor, stmt string, args ...any) (pgconn.CommandTag, error) {
+	return executor.Exec(ctx, stmt, args...)
 }
